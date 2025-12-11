2d9c59a4265ce48c657a62c1594e593ed85ebd41
MBThePro <131933737+MBThePro@users.noreply.github.com>
2025-12-11 16:33:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# c14220065_uas_notes_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/c14220065_uas_notes_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.c14220065_uas_notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
